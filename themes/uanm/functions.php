ae87c9f6e32955891b1be012e6031d1d16352625
Michael Beckwith <tw2113@gmail.com>
2021-09-12 18:39:02 -0500

    more touchups

--- themes/uanm/functions.php
+++ themes/uanm/functions.php
@@ -38,7 +38,7 @@ function uanm_load_scripts() {
 	wp_enqueue_style( 'style', get_bloginfo( 'stylesheet_url' ), 'normalize', null, 'all' );
 
 	if ( wp_is_mobile() ) {
-		wp_enqueue_script( 'mobile-menu', get_stylesheet_directory_uri() . '/js/mobile-menu.js', [], '1.0.0', true );
+		#wp_enqueue_script( 'mobile-menu', get_stylesheet_directory_uri() . '/js/mobile-menu.js', [], '1.0.0', true );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'uanm_load_scripts' );
