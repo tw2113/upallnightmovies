6018c95189f8444050f1e1c8c172a4257795150b
Michael Beckwith <tw2113@gmail.com>
2021-11-20 14:04:18 -0600

    add a matomo campaign param to RSS feeds

--- themes/uanm/functions.php
+++ themes/uanm/functions.php
@@ -379,3 +379,8 @@ function uanm_default_notification() {
     );
 }
 #add_action( 'uanm_notification', 'uanm_default_notification' );
+
+function uanm_rss_campaign( $permalink ) {
+    return $permalink . '?mtm_campaign=rss';
+}
+add_filter( 'the_permalink_rss', 'uanm_rss_campaign', 10, 1 );
