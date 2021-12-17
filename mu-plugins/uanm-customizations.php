dacdb7264539416ff5247fe2e52d3079ac996355
Michael Beckwith <tw2113@gmail.com>
2021-12-17 00:30:19 -0600

    atom feed. Closes #9

--- mu-plugins/uanm-customizations.php
+++ mu-plugins/uanm-customizations.php
@@ -9,3 +9,16 @@ function svg_favicon() {
 }
 add_action( 'wp_head', __NAMESPACE__ . '\svg_favicon' );
 add_action( 'admin_head', __NAMESPACE__ . '\svg_favicon' );
+
+function atom_links() {
+	$tmpl = '<link rel="%s" type="%s" title="%s" href="%s" />';
+
+	printf(
+		$tmpl,
+		esc_attr( 'alternate' ),
+		esc_attr( 'application/atom+xml' ),
+		esc_attr( get_bloginfo( 'name' ) . ' &raquo; Atom Feed link'  ),
+		get_bloginfo( 'atom_url' )
+	);
+}
+add_action( 'wp_head', __NAMESPACE__ . '\atom_links' );
