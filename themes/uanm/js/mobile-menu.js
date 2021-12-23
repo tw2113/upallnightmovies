3c4d3157fc916095137bf2934d4dec0ae31a15ee
Michael Beckwith <tw2113@gmail.com>
2021-12-23 17:47:25 -0600

    wait till DOM is ready

--- themes/uanm/js/mobile-menu.js
+++ themes/uanm/js/mobile-menu.js
@@ -1,21 +1,23 @@
 (function () {
-    const body = document.querySelector('body');
-    const fakeButton = document.querySelector('[data-menu-button]');
-    const menu = document.querySelector('#access');
+    document.addEventListener('DOMContentLoaded', function () {
+        const body = document.querySelector('body');
+        const fakeButton = document.querySelector('[data-menu-button]');
+        const menu = document.querySelector('#access');
 
-    const toggleMenuButton = document.createElement('button');
-    toggleMenuButton.textContent = fakeButton.textContent;
-    toggleMenuButton.setAttribute('aria-expanded', false);
-    toggleMenuButton.setAttribute('aria-controls', 'menu');
-    toggleMenuButton.classList.add('mobile-nav-toggle');
+        const toggleMenuButton = document.createElement('button');
+        toggleMenuButton.textContent = fakeButton.textContent;
+        toggleMenuButton.setAttribute('aria-expanded', false);
+        toggleMenuButton.setAttribute('aria-controls', 'menu');
+        toggleMenuButton.classList.add('mobile-nav-toggle');
 
-    fakeButton.parentNode.replaceChild(toggleMenuButton, fakeButton);
+        fakeButton.parentNode.replaceChild(toggleMenuButton, fakeButton);
 
-    toggleMenuButton.addEventListener('click', function () {
-        let expanded = this.getAttribute('aria-expanded') === 'true' || false;
-        this.setAttribute('aria-expanded', !expanded);
-        menu.hidden = !menu.hidden;
-    });
+        toggleMenuButton.addEventListener('click', function () {
+            let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+            this.setAttribute('aria-expanded', !expanded);
+            menu.hidden = !menu.hidden;
+        });
 
-    menu.hidden = true;
+        menu.hidden = true;
+    }, false);
 })();
