8deffca83146f82180a60d4e840ea495f875574d
Michael Beckwith <tw2113@gmail.com>
2021-12-23 11:18:58 -0600

    set an initial scale

--- themes/uanm/header.php
+++ themes/uanm/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="description" content="<?php bloginfo('description'); ?>" />
 
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
