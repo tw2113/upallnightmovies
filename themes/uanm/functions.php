a1ef5c227c60f291edde06654a981939db1fe3ea
Michael Beckwith <tw2113@gmail.com>
2022-01-01 10:08:39 -0600

    yay Creative Commons!

--- themes/uanm/functions.php
+++ themes/uanm/functions.php
@@ -72,6 +72,8 @@ function uanm_posted_in() {
 
 	$posted_in[] = 'Bookmark the <a class="u-url" href="' . esc_url( get_the_permalink() ) . '" rel="bookmark">permalink</a>.';
 
+    $posted_in[] = 'This post has a <a class="cclicense" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a> <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>';
+
 	echo implode( ', ', $posted_in );
 }
 
