c535f32a5019d98d8042c160e8cd6ba77a4aa367
Michael Beckwith <tw2113@gmail.com>
2022-02-07 15:55:57 -0600

    frontpage juggling

--- themes/uanm/front-page.php
+++ themes/uanm/front-page.php
@@ -2,62 +2,66 @@
 
 <?php
 	global $wp_query;
-	if ( $wp_query->max_num_pages > 1 ) :
-        echo '<div class="posts-nav header">';
-		next_posts_link( '&larr; Older posts' );
-		previous_posts_link( 'Newer posts &rarr;' );
-		echo '</div>';
-	endif;
 ?>
 <div class="content" role="main">
+    <h2>Movies</h2>
+    <div class="movies">
 	<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-	<article role="article" <?php post_class( 'h-entry' ); ?>>
-			<header>
-			<h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<?php uanm_posted_on(); ?>
-			</header>
-
-			<?php
-			$has_poll = has_category( 'poll' );
-			$featured_cats = [ 'frontpage-featured' ];
-			if ( $has_poll ) {
-				$featured_cats[] = 'poll';
-			}
-			?>
-
+	    <article role="article" <?php post_class( 'h-entry' ); ?>>
 			<div class="front-archive-wrap">
-				<?php
-				if ( has_post_thumbnail() || has_category( 'poll' ) ) {
-					?>
-					<div class="<?php echo esc_attr( implode( ' ', $featured_cats ) ); ?>">
-						<?php
-						if ( has_post_thumbnail() ) {
-							the_post_thumbnail( 'medium' );
-						} else if ( has_category( 'poll' ) ) {
-							echo uanm_poll_thumbnail();
-						}
-						?>
-					</div>
-					<?php
-				}
-			if ( ! has_category( 'poll' ) ) {
-				echo apply_filters( 'the_excerpt', wpautop( get_the_excerpt() ) );
-			} else {
-				the_content();
-			}
-			?>
-
+				<div class="<?php echo esc_attr( implode( ' ', [ 'frontpage-featured' ] ) ); ?>">
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium' ); ?></a>
+                </div>
 			</div>
 		</article>
 		<?php endwhile; endif; ?>
+    </div>
+	<?php
+		if (  $wp_query->max_num_pages > 1 ) :
+			echo '<div class="posts-nav footer">';
+			previous_posts_link( '&larr; Recent movies' );
+			next_posts_link( 'Past movies &rarr;' );
+			echo '</div>';
+		endif;
+	?>
 
-        <?php
-	        if (  $wp_query->max_num_pages > 1 ) :
-		        echo '<div class="posts-nav footer">';
-		        next_posts_link( '&larr; Older posts' );
-		        previous_posts_link( 'Newer posts &rarr;' );
-		        echo '</div>';
-	        endif;
-        ?>
+        <h2>Polls and Trailers</h2>
+        <div class="polls-trailers">
+            <?php
+                $args = [
+                    'category__in' => [
+                        21,
+                        40,
+                    ],
+                    'posts_per_page' => 3
+                ];
+                $polls_trailers = new WP_Query( $args );
+
+            while( $polls_trailers->have_posts() ) : $polls_trailers->the_post();
+            ?>
+            <article role="article" <?php post_class( 'h-entry' ); ?>>
+                <header>
+                   <h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                </header>
+
+                <?php
+                    $has_poll = has_category( 'poll' );
+                    $featured_cats = [ 'frontpage-featured' ];
+                    if ( $has_poll ) {
+                        $featured_cats[] = 'poll';
+                    }
+                ?>
+
+                <div class="front-archive-wrap">
+                    <div class="<?php echo esc_attr( implode( ' ', $featured_cats ) ); ?>">
+                        <?php echo uanm_poll_thumbnail(); ?>
+                    </div>
+                </div>
+
+            </article>
+            <?php
+                endwhile; wp_reset_postdata();
+            ?>
+        </div>
 </div>
 <?php get_footer(); ?>
